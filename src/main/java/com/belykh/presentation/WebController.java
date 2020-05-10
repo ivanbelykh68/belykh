f733370ab880a9ed8978e1529e5765e1f887e16a
Ivan Belykh <ivan.belykh@gmail.com>
2020-05-10 12:48:44 +0300

    Web UI

---
+++ src/main/java/com/belykh/presentation/WebController.java
@@ -0,0 +1,14 @@
+package com.belykh.presentation;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class WebController {
+	@GetMapping
+	public String index(){
+		return "index";
+	}
+}
